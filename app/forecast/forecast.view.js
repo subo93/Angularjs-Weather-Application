3d019bec25c53ef5bd43406ed7168e6c1bea92d7
subo93 <nsubodhi@gmail.com>
2018-01-11 16:26:19 +0530

    upto chart

--- app/forecast/forecast.view.js
+++ app/forecast/forecast.view.js
@@ -1,18 +1,25 @@
 angular.module("app.forecast")
-.directive(myForecast,function () {
-    cosole.log( "start directice!!!!!!!!!");
+.directive("myforecast",function () {
+    console.log( "start directice!!!!!!!!!");
     return {
-       // restrict:"AE",
+        restrict:"AEC",
         replace:true,
         scope:{
+            //data provider
             dp:"="
         },
-        template:"<div id='contianer'></div>",
+        template:"<div id='container'></div>",
         link:linkFn
-    }
-    function linkFn(scope,element,attrs) {
-        cosole.log( element);
-        coonsole.log("directive after log!!!!");
 
     }
-});
+  //  console.log("before link function!!!");
+    function linkFn(scope,element,attrs) {
+        console.log("directive before log!!!!");
+        console.log("neeeddddddd "+attrs);
+        console.log("directive after log!!!!");
+
+    }
+        console.log("After linkkkk");
+}
+
+);
